993f3ddd01f6fb1fd83fc0a01ec6210c0758e2f2
shshaheen <kolimishaheen9885@gmail.com>
2025-02-07 23:22:33 +0530

    Taken Feedback and added to the cloud

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,17 @@
 // Copyright 2022 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider;    // new
-import 'package:flutter/material.dart';           // new
-import 'package:provider/provider.dart';          // new
 
-import 'app_state.dart';                          // new
-import 'src/authentication.dart';                 // new
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'app_state.dart'; 
+import 'guest_book.dart';
 
+import 'src/authentication.dart';
 import 'src/widgets.dart';
+import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,6 +47,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
